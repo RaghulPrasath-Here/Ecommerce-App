60a3bb60add61364d4f1bec2dcd43b21027489d1
RaghulPrasath-Here <raghulprasathhere@gmail.com>
2025-05-24 23:37:23 +0100

    CategoryDto object created

--- src/main/java/com/rodi/Ecommerce_App/payload/CategoryDTO.java
+++ src/main/java/com/rodi/Ecommerce_App/payload/CategoryDTO.java
@@ -1,4 +1,19 @@
 package com.rodi.Ecommerce_App.payload;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryDTO {
+
+    private Long categoryId;
+    @NotBlank
+    @Size(min = 5, message = "Category name size should be greater than 5")
+    private String categoryName;
 }
+
