c38572cf436beebb9abe27ff795e491836600073
RaghulPrasath-Here <raghulprasathhere@gmail.com>
2025-06-07 23:57:22 +0100

    chore : roles & address set created for user

--- src/main/java/com/rodi/Ecommerce_App/model/User.java
+++ src/main/java/com/rodi/Ecommerce_App/model/User.java
@@ -45,6 +45,22 @@ public class User {
         this.password = password;
     }
 
+    @Setter
+    @Getter
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+            fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    @Getter
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "user_address",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "address_id"))
+    private List<Address> addresses = new ArrayList<>();
+    
     @ToString.Exclude
     @OneToMany(mappedBy = "user",
                 cascade = {CascadeType.PERSIST, CascadeType.MERGE},
