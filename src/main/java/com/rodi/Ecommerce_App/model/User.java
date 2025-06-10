18cf2057a32330a8bb9daebc864c6cb20ee75fa0
RaghulPrasath-Here <raghulprasathhere@gmail.com>
2025-06-10 23:55:13 +0100

    springSecurity JwtUtils executed

--- src/main/java/com/rodi/Ecommerce_App/model/User.java
+++ src/main/java/com/rodi/Ecommerce_App/model/User.java
@@ -60,7 +60,7 @@ public class User {
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "address_id"))
     private List<Address> addresses = new ArrayList<>();
-    
+
     @ToString.Exclude
     @OneToMany(mappedBy = "user",
                 cascade = {CascadeType.PERSIST, CascadeType.MERGE},
