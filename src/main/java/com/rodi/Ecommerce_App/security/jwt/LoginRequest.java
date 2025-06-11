3890635ab50e601ca1c879fef98569871444f6d9
RaghulPrasath-Here <raghulprasathhere@gmail.com>
2025-06-11 23:26:20 +0100

    loginRequest and loginResponse objext created

---
+++ src/main/java/com/rodi/Ecommerce_App/security/jwt/LoginRequest.java
@@ -0,0 +1,23 @@
+package com.rodi.Ecommerce_App.security.jwt;
+
+public class LoginRequest {
+    private String username;
+
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
